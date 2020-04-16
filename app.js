094c768542ae8b967da44853f5e740892b330a6d
itzfyp <itz.fyp@gmail.com>
2020-04-16 16:29:00 +0530

    node setup for docker demo

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+// Allows to access static files from given folder
+const publicFolderAccess = express.static(path.join(__dirname, 'public'));
+app.use(publicFolderAccess);
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  /* const filePath = path.join(__dirname, 'views', '404.html');
+  res.status(404).sendFile(filePath); */
+  res.statusCode = 404;
+  res.render('404', {
+    pageTitle: 'Page not found'
+  });
+});
+
+// App Server starts here
+app.listen(8080, '0.0.0.0')
